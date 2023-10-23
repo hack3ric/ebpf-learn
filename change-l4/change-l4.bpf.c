00bab826515ee7e41deca680231db2038cb62d74
Eric Long <i@hack3r.moe>
2023-10-23 17:53:59 +0800

    change-l4: Add UDP csum & IPv6

--- change-l4/change-l4.bpf.c
+++ change-l4/change-l4.bpf.c
@@ -7,75 +7,152 @@
 #include <linux/ipv6.h>
 #include <linux/pkt_cls.h>
 #include <linux/udp.h>
+
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 
-#define shift_ptr(type, ret, nhptr, data_end) ({ \
-  type* result = nhptr; \
-  nhptr += sizeof(type); \
-  if (nhptr > data_end) {  \
-    return ret; \
-  } \
-  result; \
-})
+#define check_decl(type, name, off, skb, ret)                           \
+  type* name = ({                                                       \
+    type* ptr = (void*)(size_t)skb->data + off;                         \
+    if ((size_t)ptr + sizeof(type) > (size_t)skb->data_end) return ret; \
+    ptr;                                                                \
+  })
 
-#define shift_ptr_decl(type, name, nhptr, skb) \
-  type* name = shift_ptr(type, TC_ACT_OK, nhptr, (void*)(long)skb->data_end);
+#define check_decl_unspec(type, name, off, skb) \
+  check_decl(type, name, off, skb, TC_ACT_UNSPEC)
 
-static inline int handle_ipv4(struct iphdr* ipv4, void** nhptr, struct __sk_buff* skb) {
-  if (ipv4->protocol != IPPROTO_ICMP) return -1;
+#define ETH_END (ETH_HLEN)
+#define IPV4_END (ETH_END + sizeof(struct iphdr))
+#define IPV6_END (ETH_END + sizeof(struct ipv6hdr))
+#define IPV4_UDP_END (IPV4_END + sizeof(struct udphdr))
+#define IPV6_UDP_END (IPV6_END + sizeof(struct udphdr))
 
-  shift_ptr_decl(struct icmphdr, icmp, *nhptr, skb);
-  __bpf_printk("%pI4 -> %pI4", &ipv4->saddr, &ipv4->daddr);
-  __bpf_printk("type %d, code %d", icmp->type, icmp->code);
+static void update_csum(__u16* csum, __s32 delta) {
+  __u32 new_csum = (__u16) ~*csum + delta;
+  for (int i = 0; i < 3; i++) {
+    __u16 hi = new_csum >> 16, lo = new_csum & 0xffff;
+    if (!hi) break;
+    new_csum = hi + lo;
+  }
+  *csum = ~new_csum;
+}
+
+static void update_csum_ipv4_pseudo(__u16* csum, struct iphdr* ipv4) {
+  __u32 saddr = bpf_ntohl(ipv4->saddr);
+  __u32 daddr = bpf_ntohl(ipv4->daddr);
+  __u16 l4_len = bpf_ntohs(ipv4->tot_len) - (ipv4->ihl << 2);
+
+  update_csum(csum, saddr >> 16);
+  update_csum(csum, saddr & 0xffff);
+  update_csum(csum, daddr >> 16);
+  update_csum(csum, daddr & 0xffff);
+  update_csum(csum, ipv4->protocol);
+  update_csum(csum, l4_len);
+}
+
+static void update_csum_ipv6_pseudo(__u16* csum, struct ipv6hdr* ipv6) {
+  for (int i = 0; i < 8; i++) {
+    update_csum(csum, bpf_ntohs(ipv6->saddr.in6_u.u6_addr16[i]));
+    update_csum(csum, bpf_ntohs(ipv6->daddr.in6_u.u6_addr16[i]));
+  }
+  update_csum(csum, bpf_ntohs(ipv6->payload_len));
+  update_csum(csum, ipv6->nexthdr);
+}
+
+static void update_csum_udp_header(__u16* csum, struct udphdr* udp) {
+  update_csum(csum, bpf_ntohs(udp->source));
+  update_csum(csum, bpf_ntohs(udp->dest));
+  update_csum(csum, bpf_ntohs(udp->len));
+}
+
+static void update_csum_data(struct __sk_buff* skb, __u16* csum, __u32 off) {
+  __u16* data = (void*)(size_t)skb->data + off;
+  for (int i = 0; i < ETH_DATA_LEN / 2; i++) {
+    if ((size_t)(data + i + 1) > (size_t)skb->data_end) break;
+    update_csum(csum, bpf_ntohs(data[i]));
+  }
+}
+
+static int handle_ipv4(struct __sk_buff* skb, struct iphdr* ipv4) {
+  if (ipv4->protocol != IPPROTO_ICMP) return TC_ACT_OK;
+  check_decl_unspec(struct icmphdr, icmp, IPV4_END, skb);
 
   // We only modify echo request
-  if (icmp->type != ICMP_ECHO) return -1;
+  if (icmp->type != ICMP_ECHO) return TC_ACT_OK;
 
   __u16 l4_len = bpf_ntohs(ipv4->tot_len) - (ipv4->ihl << 2);
-  __bpf_printk("l4_len = %d", l4_len);
-  __u16 sport = 11451;
-  __u16 dport = 41919;
+  __u16 sport = 11451, dport = 41919;
 
   // Change L4 header from ICMP to UDP
-  *nhptr -= sizeof(struct icmphdr);
-  shift_ptr_decl(struct udphdr, udp, *nhptr, skb);
+  check_decl_unspec(struct udphdr, udp, IPV4_END, skb);
   udp->source = bpf_htons(sport);
   udp->dest = bpf_htons(dport);
   udp->len = bpf_htons(l4_len);
-  udp->check = 0;
-
-  // TODO: calculate UDP checksum
-  __sum16 udp_csum = 0;
-  udp->check = udp_csum;
 
   __u8 old_protocol = ipv4->protocol;
   ipv4->protocol = IPPROTO_UDP;
-  bpf_l3_csum_replace(skb, (__u32)ipv4 - skb->data + offsetof(struct iphdr, check),
-    bpf_htons(old_protocol), bpf_htons(ipv4->protocol), 2);
 
-  return 0;
+  __u16 udp_csum = 0xffff;
+  update_csum_ipv4_pseudo(&udp_csum, ipv4);
+  update_csum_udp_header(&udp_csum, udp);
+  update_csum_data(skb, &udp_csum, IPV4_UDP_END);
+  udp->check = bpf_htons(udp_csum);
+
+  bpf_l3_csum_replace(
+      skb, (__u32)(size_t)ipv4 - skb->data + offsetof(struct iphdr, check),
+      bpf_htons(old_protocol), bpf_htons(ipv4->protocol), 2);
+
+  return TC_ACT_OK;
+}
+
+static int handle_ipv6(struct __sk_buff* skb, struct ipv6hdr* ipv6) {
+  if (ipv6->nexthdr != IPPROTO_ICMPV6) return TC_ACT_OK;
+  check_decl_unspec(struct icmp6hdr, icmpv6, IPV6_END, skb);
+
+  // We only modify echo request
+  if (icmpv6->icmp6_type != ICMPV6_ECHO_REQUEST) return TC_ACT_OK;
+  __u16 l4_len = bpf_ntohs(ipv6->payload_len);
+  __u16 sport = 11451, dport = 41919;
+
+  // Change L4 header from ICMP to UDP
+  check_decl_unspec(struct udphdr, udp, IPV6_END, skb);
+  udp->source = bpf_htons(sport);
+  udp->dest = bpf_htons(dport);
+  udp->len = bpf_htons(l4_len);
+
+  __u8 old_protocol = ipv6->nexthdr;
+  ipv6->nexthdr = IPPROTO_UDP;
+
+  __u16 udp_csum = 0xffff;
+  update_csum_ipv6_pseudo(&udp_csum, ipv6);
+  update_csum_udp_header(&udp_csum, udp);
+  update_csum_data(skb, &udp_csum, IPV6_UDP_END);
+  udp->check = bpf_htons(udp_csum);
+
+  // IPv6 has no checksum
+
+  return TC_ACT_OK;
 }
 
 SEC("classifier")
 int change_l4(struct __sk_buff* skb) {
-  void* nhptr = (void*)(long)skb->data;
-
-  shift_ptr_decl(struct ethhdr, eth, nhptr, skb);
+  check_decl_unspec(struct ethhdr, eth, 0, skb);
   switch (bpf_ntohs(eth->h_proto)) {
     case 0x0800: /* IPv4 */ {
-      shift_ptr_decl(struct iphdr, ipv4, nhptr, skb);
-      if (handle_ipv4(ipv4, &nhptr, skb)) return TC_ACT_OK;
+      check_decl_unspec(struct iphdr, ipv4, ETH_END, skb);
+      int result = handle_ipv4(skb, ipv4);
+      if (!result) return result;
       break;
     }
     case 0x86dd: /* IPv6 */ {
-      shift_ptr_decl(struct ipv6hdr, ipv6, nhptr, skb);
+      check_decl_unspec(struct ipv6hdr, ipv6, ETH_END, skb);
+      int result = handle_ipv6(skb, ipv6);
+      if (!result) return result;
       break;
     }
-    default: // We don't know about/process this packet
+    default:  // We don't know about/process this packet
       return TC_ACT_OK;
   }
-
   return TC_ACT_OK;
 }
 
